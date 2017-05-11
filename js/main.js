22684981ee8988e73577c8f6fcaf854bf00054a7
desireesanchez <dez@Desirees-MacBook-Pro.local>
2017-05-11 20:45:33 +0800

    home page styles

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+   // jQuery methods go here...
+   $(".typing").typed({
+     strings: ["Make Nadine MyRealtor, Yours."],
+     typeSpeed: 50,
+     showCursor: false
+   });
+});
